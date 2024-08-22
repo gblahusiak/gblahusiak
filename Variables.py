fb7ca00172dadad93433c115c2524548b96bb5cb
gblahusiak <gblahusiak@tulane.edu>
2024-08-22 11:45:08 -0500

    Create Variables.py
    Variables practice problem converting degrees in Celsius to degrees in Farenheit.

---
+++ Variables.py
@@ -0,0 +1,5 @@
+temperature_c = input("What is the temperature in Celsius?")
+temperature_c_float = float(temperature_c)
+temperature_f = temperature_c_float * 9/5 + 32
+temperature_f_float = float(temperature_f)
+print(temperature_c_float,"degrees C is",temperature_f_float,"degrees F.")
